34512b08c206958a8ff7298a34033f697dc46efa
Septs <github@septs.pw>
2022-05-06 16:35:19 +0800

    improve kv service

--- src/kv/service.ts
+++ src/kv/service.ts
@@ -1,5 +1,5 @@
 import { KVClient } from './client'
-import { BaseInfo, SetOptions } from './types'
+import { BaseInfo } from './types'
 
 export interface KVServiceOptions extends BaseInfo {
   readonly client: KVClient
@@ -22,21 +22,25 @@ export class KVService {
     return this.client.get(this.persona)
   }
 
-  getPayload<Patch>(patch: Patch) {
-    return this.client.getPayload<Patch>({
+  async set<Patch>(patch: Patch, options: SignatureOptions) {
+    const payload = await this.client.getPayload<Patch>({
       persona: this.persona,
       platform: this.platform,
       identity: this.identity,
       patch,
     })
-  }
-
-  set<Patch>(options: Omit<SetOptions<Patch>, keyof BaseInfo>) {
-    return this.client.set({
+    return this.client.set<Patch>({
       persona: this.persona,
       platform: this.platform,
       identity: this.identity,
-      ...options,
+      uuid: payload.uuid,
+      created_at: payload.created_at,
+      signature: await options.onSignature(payload.sign_payload),
+      patch,
     })
   }
+}
+
+interface SignatureOptions {
+  onSignature(payload: string): string | Promise<string>
 }
